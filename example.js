ea828b7509574d7b467728d1b6768a371d088a92
doolgi158 <kolonel@hanmail.net>
2025-07-11 17:22:58 +0900

    기능 수정

---
+++ example.js
@@ -0,0 +1,26 @@
+let string;
+
+document.getElementById("image1").addEventListener("click", () => {
+    string = "";
+    string = document.getElementById("extra").innerHTML = "<div>asdfasdfasdf<br/>asdfasdfasdfasdf<br/>asdfasdfasdfsadf</div>"
+})
+
+document.getElementById("image2").addEventListener("click", () => {
+    string = "";
+    string = document.getElementById("extra").innerHTML = "<div>164896<br/>879846<br/>65165156</div>"
+})
+
+document.getElementById("image3").addEventListener("click", () => {
+    string = "";
+    string = document.getElementById("extra").innerHTML = "<div>164afdgserftgsdfhsdh896<br/>879846<br/>65165156</div>"
+})
+
+document.getElementById("image4").addEventListener("click", () => {
+    string = "";
+    string = document.getElementById("extra").innerHTML = "<div>16468+586+78+7864896<br/>ghsghfghdfghdfghfdghdfghdfg<br/>65165156</div>"
+})
+
+document.getElementById("image5").addEventListener("click", () => {
+    string = "";
+    string = document.getElementById("extra").innerHTML = "<div>1786+879687664896<br/>879846<br/>651651hfdghdfghdfghdfghdfghdfgh56</div>"
+})
