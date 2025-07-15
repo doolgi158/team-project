26d8f324b4ae784640741437a4440e86fb9e1cb2
doolgi <kolonel@hanmail.net>
2025-07-15 23:27:44 +0900

    완성본

--- rhythm.js
+++ rhythm.js
@@ -15,7 +15,7 @@ document.getElementById("image1").addEventListener("click", () => {
             <iframe width='450' height='300' src='https://www.youtube.com/embed/FISrD1fEEo8?si=Fe-5NwpHUYQX5SIj' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share' referrerpolicy='strict-origin-when-cross-origin' allowfullscreen></iframe>\
         </li>\
     </ul>"
-})
+});
 
 document.getElementById("image2").addEventListener("click", () => {
     string = "";
@@ -39,15 +39,37 @@ document.getElementById("image2").addEventListener("click", () => {
 
 document.getElementById("image3").addEventListener("click", () => {
     string = "";
-    string = document.getElementById("extra").innerHTML = "<div>164afdgserftgsdfhsdh896<br/>879846<br/>65165156</div>"
+    string = document.getElementById("extra").innerHTML = "<div class='head'>개임 소개</div><hr />\
+    호주의 peppy라는 개발자가 응원단 시리즈의 게임 방식을 따와서 만든 오프라인&온라인 리듬 게임. osu!라는 제목은 응원단 시리즈의 응원구호인 '옷스!(押忍!)' 에서 따온 것이다.<br />\
+    현재는 주객이 완전히 전도되어서 응원단 시리즈는 몰라도 이 게임은 아는 사람들이 많아졌으며 osu!가 응원단 시리즈의 정식 후속작이라 생각하는 사람은 물론 응원단 시리즈가 osu!의 짝퉁이라고 생각하는 사람도 늘어났다. 이러한 점은 아브의 응원단 1편 실황 플레이 영상 코멘트에서 잘 드러났다. 이는 원작인 응원단 시리즈가 2007년 응원단2를 끝으로 더 안나오기에 게임 인지도가 갈수록 잊혀져가는 것도 한몫했다.<br />\
+    <hr />\
+    <div class='head'>관련 영상</div>\
+    <hr />\
+    <ul class='cover'>\
+        <li>\
+            <iframe width='450' height='300' src='https://www.youtube.com/embed/336BlGiQar4?si=SCHLSHTRHAYe0f3o' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share' referrerpolicy='strict-origin-when-cross-origin' allowfullscreen></iframe>\
+            <iframe width='450' height='300' src='https://www.youtube.com/embed/OvCbKP_ox7o?si=WSGiyt2jarPNcE3o' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share' referrerpolicy='strict-origin-when-cross-origin' allowfullscreen></iframe>\
+            <iframe width='450' height='300' src='https://www.youtube.com/embed/l-VPCGl0qy4?si=sDp82grjG4sdioip' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share' referrerpolicy='strict-origin-when-cross-origin' allowfullscreen></iframe>\
+        </li>\
+    </ul>"
 })
 
 document.getElementById("image4").addEventListener("click", () => {
     string = "";
-    string = document.getElementById("extra").innerHTML = "<div>16468+586+78+7864896<br/>ghsghfghdfghdfghfdghdfghdfg<br/>65165156</div>"
+    string = document.getElementById("extra").innerHTML = "<div class='head'>개임 소개</div><hr />\
+    A Dance of Fire and Ice를 제작한 7th Beat Games에서 제작한 리듬 게임. 2021년 2월 26일에 스팀에서 앞서 해보기로 출시되었다. BIC나 G-STAR에서도 출품되었으며 공식적으로 한국어를 지원한다.<br />\
+    <hr />\
+    <div class='head'>관련 영상</div>\
+    <hr />\
+    <ul class='cover'>\
+        <li>\
+            <iframe width='450' height='300' src='https://www.youtube.com/embed/mV7p1NXEmB0?si=fEOCHg8icAbz2M84' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share' referrerpolicy='strict-origin-when-cross-origin' allowfullscreen></iframe>\
+            <iframe width='450' height='300' src='https://www.youtube.com/embed/xMz2dHgKZdw?si=wSGnbUsib-OzMXy7' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share' referrerpolicy='strict-origin-when-cross-origin' allowfullscreen></iframe>\
+            <iframe width='450' height='300' src='https://www.youtube.com/embed/gBcvKobh9N0?si=Qcg45ORQoeiuucAK' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share' referrerpolicy='strict-origin-when-cross-origin' allowfullscreen></iframe>\
+        </li>\
+    </ul>"
 })
 
-document.getElementById("image5").addEventListener("click", () => {
-    string = "";
-    string = document.getElementById("extra").innerHTML = "<div>1786+879687664896<br/>879846<br/>651651hfdghdfghdfghdfghdfghdfgh56</div>"
+document.getElementsByName("rhythm").addEventListener("click", () => {
+    href = "rhythm.html";
 })
