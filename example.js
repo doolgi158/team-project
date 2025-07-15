2cc7a41fcacb89830e8141a21fe59b13b588c0a5
doolgi158 <kolonel@hanmail.net>
2025-07-15 17:50:53 +0900

    djmax, 얼불춤, 오수 내용 추가

--- example.js
+++ example.js
@@ -2,12 +2,36 @@ let string;
 
 document.getElementById("image1").addEventListener("click", () => {
     string = "";
-    string = document.getElementById("extra").innerHTML = "<div>asdfasdfasdf<br/>asdfasdfasdfasdf<br/>asdfasdfasdfsadf</div>"
+    string = document.getElementById("extra").innerHTML = "<div class='head'>개임 소개</div><hr />\
+    <div>DJMAX RESPECT V는 한국의 로키 스튜디오에서 개발하고 네오위즈에서 퍼블리싱하는 DJMAX 시리즈의 PC 리듬게임으로, 2017년 PS4 플랫폼으로 출시된 DJMAX RESPECT에 추가 콘텐츠를 포함시킨 이식작이다. 공식 약칭은 DMRV이다.<br />\
+    2019년 11월 18일 최초 공개되어 2019년 12월 19일 얼리 액세스를 시작했으며, 2020년 3월 12일 정식 서비스를 시작했다. 2022년 7월 7일 Xbox Game Pass 플랫폼에도 정식 출시되었다.</div>\
+    <hr />\
+    <div class='head'>관련 영상</div>\
+    <hr />\
+    <ul class='cover'>\
+        <li>\
+            <iframe width='450' height='300' src='https://www.youtube.com/embed/IkygdWqvOV8?si=Tr7GZ_gbTyDPywD0' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share' referrerpolicy='strict-origin-when-cross-origin' allowfullscreen></iframe>\
+            <iframe width='450' height='300' src='https://www.youtube.com/embed/azWYOG8yXQw?si=nTGp6d7ER19Yijh1' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share' referrerpolicy='strict-origin-when-cross-origin' allowfullscreen></iframe>\
+            <iframe width='450' height='300' src='https://www.youtube.com/embed/FISrD1fEEo8?si=Fe-5NwpHUYQX5SIj' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share' referrerpolicy='strict-origin-when-cross-origin' allowfullscreen></iframe>\
+        </li>\
+    </ul>"
 })
 
 document.getElementById("image2").addEventListener("click", () => {
     string = "";
-    string = document.getElementById("extra").innerHTML = "<div>164896<br/>879846<br/>65165156</div>"
+    string = document.getElementById("extra").innerHTML = "<div class='head'>개임 소개</div><hr />\
+    호주의 peppy라는 개발자가 응원단 시리즈의 게임 방식을 따와서 만든 오프라인&온라인 리듬 게임. osu!라는 제목은 응원단 시리즈의 응원구호인 '옷스!(押忍!)' 에서 따온 것이다.<br />\
+    현재는 주객이 완전히 전도되어서 응원단 시리즈는 몰라도 이 게임은 아는 사람들이 많아졌으며 osu!가 응원단 시리즈의 정식 후속작이라 생각하는 사람은 물론 응원단 시리즈가 osu!의 짝퉁이라고 생각하는 사람도 늘어났다. 이러한 점은 아브의 응원단 1편 실황 플레이 영상 코멘트에서 잘 드러났다. 이는 원작인 응원단 시리즈가 2007년 응원단2를 끝으로 더 안나오기에 게임 인지도가 갈수록 잊혀져가는 것도 한몫했다.<br />\
+    <hr />\
+    <div class='head'>관련 영상</div>\
+    <hr />\
+    <ul class='cover'>\
+        <li>\
+            <iframe width='450' height='300' src='https://www.youtube.com/embed/p9NuERYrzIo?si=SIaC3xBMAI-8cLi_' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share' referrerpolicy='strict-origin-when-cross-origin' allowfullscreen></iframe>\
+            <iframe width='450' height='300' src='https://www.youtube.com/embed/FDHxZnTpmpk?si=9Xi4CSE_986pPx_w' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share' referrerpolicy='strict-origin-when-cross-origin' allowfullscreen></iframe>\
+            <iframe width='450' height='300' src='https://www.youtube.com/embed/jxYV-kdt3H0?si=Gmk4B-qdlIpyuNJ0' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share' referrerpolicy='strict-origin-when-cross-origin' allowfullscreen></iframe>\
+        </li>\
+    </ul>"
 })
 
 document.getElementById("image3").addEventListener("click", () => {
