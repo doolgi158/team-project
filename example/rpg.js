0290e6b70bcaa2ec506b390a3456fda008c2f20d
YHee88 <isitets@naver.com>
2025-07-15 19:13:14 +0900

    페이지간에 서로 이동네비게이션추가 클릭시 설명시 동영상까지 추가

---
+++ example/rpg.js
@@ -0,0 +1,40 @@
+let string;
+
+document.getElementById("rpg1").addEventListener("click", () => {
+    document.getElementById("extra").innerHTML = 
+    "<head>와우는 접는 것이 아니라 잠깐 쉬는 것이다</head><br />\
+    <div>20여년이 넘은 모든 RPG의 시초이자 조상격인 게임이다. 한국에서도 게임업계뿐 아니라 문화, 언어 사회 등 여러 영향을 미쳤다. 20여년의 업데이트 동안 세계관이 확장되고 커지면서 신규 유저들이 알기 어렵다는 단점이 있지만 이 단점을 압도할 정도의 재미가 있다. 괜히 '와우는 접는게 아니라 쉬는 것이다'라는 말이 나오는 것이 아니다. RPG를 해보고 싶다면 강력하게 추천한다.</div>"
+
+    document.getElementById("video").innerHTML =
+    `<iframe width="560" height="315" src="https://www.youtube.com/embed/S0K26sKTlT4?si=eNjnFYI8oCRr8BRs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`
+    
+});
+
+document.getElementById("rpg2").addEventListener("click", () => {
+    string = "";
+    string = document.getElementById("extra").innerHTML = 
+    "<head>압도적 완성도와 세계관</head><br /> \
+    <div>안제이 사이콥스키 작자의 동명의 판타지 소설을 원작으로 시리즈의 최종편이다. 시리즈 전통의 묵직한 서사와 성숙한 분위기를 그대로 계승하면서도 디자인 측명에서 오픈월드라는 형식을 도입해 전작들보다 훨씬 더 자유롭고 풍성한 게임 플레이를 즐길 수 있다. 플레이어는 괴물 사냥꾼인 게롤트가 되어 세계를 누비며 실종된 양녀 시리를 찾아 나서게 되며 그 과정에서 정치적 음모와 초자연적인 위협을 함께 맞닥뜨리게 된다.</div>"
+
+    document.getElementById("video").innerHTML =
+    `<iframe width="560" height="315" src="https://www.youtube.com/embed/elEI4SG5e3c?si=g-h4MMqiIB_xDDiu" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`
+});
+
+document.getElementById("rpg3").addEventListener("click", () => {
+    string = "";
+    string = document.getElementById("extra").innerHTML = 
+    "<head>PS5를 사야하는 이유!!</head><br />\
+    <div>유저가 직접 광활한 판타지 세계를 자유롭게 탐험하면서 전투와 성장 요소를 경험하는 구조로 기존 다크소울 시리즈와 유사한 핵심 시스템을 유지하면서도, 자유도 높은 탐험방식과 탈것 활용, 넓은 지형 구성이 큰 특징이다. 주인공은 타락한 땅이라는 배경 세계에서 엘든링을 되찾기 위한 여정을 떠나게 됩니다. 다양한 NPC, 보스몬스터, 던젼 시스템이 조화를 이루며 전통적인 RPG 진행방식과 차별화된 깊이를 보여줍니다. 출시 당시 이미 전 세계적인 주목을 받았고 콘솔 ㅔㅇ미 팬들 사이에서 필수 플레이 타이틀로 여겨졌습니다.</div><br />\
+    "
+    document.getElementById("video").innerHTML =
+    '<iframe width="560" height="315" src="https://www.youtube.com/embed/vZfVUe_957c?si=wHXYXdP4lV_6x8Vy" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>'
+});
+
+document.getElementById("rpg4").addEventListener("click", () => {
+    string = "";
+    string = document.getElementById("extra").innerHTML = 
+    "<head>현재 한국에서 가장 인기 있는 RPG</head><br />\
+    <div>큰 기대를 한 몸에 받고 오픈하였지만 초기 큰 어려움을 겪었던 로스트아크는 7년이 지난 현재 한국에서 가장 사랑받는 게임이 되었다. 유저들과 소통이라는 기존 K-RPG에서 보기어려웠던 부분을 내세워 잘 만든 레이드 퀄리티와 전투스타일, 다양한 컨텐츠 수집 등 RPG 본연의 재미도 챙기고 있는 로스트아크 이번 기회에 한번 '찍먹'해보자</div>"
+    document.getElementById("video").innerHTML =
+    `<iframe width="560" height="315" src="https://www.youtube.com/embed/geVlBf6bWgM?si=vFKGp-d2m_Tph-IB" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`
+});
