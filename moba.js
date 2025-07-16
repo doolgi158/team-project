6d67bac13bf8f2189a7e8f88fe1632e8ec91a7f4
YHee88 <isitets@naver.com>
2025-07-16 09:06:14 +0900

    용현씨파일

---
+++ moba.js
@@ -0,0 +1,75 @@
+let string;
+
+document.getElementById("image1").addEventListener("click", () => {
+    string = "";
+    string = document.getElementById("extra").innerHTML = "<div class='head'>개임 소개</div><hr />\
+    라이엇 게임즈에서 개발한 MOBA 게임. 게임 명칭의 앞 철자들을 따서 LoL(롤), 영어권에서는 League 등의 약칭으로도 불린다.<br />\
+    이전까지 있었던 MOBA(AOS) 게임들보다 진입 장벽을 낮추는 것으로 높은 인기를 얻었고 현재는 전 세계를 통틀어 굉장히 많은 유저들을 보유 중이다. PC 게임 중 전 세계에서 가장 많이 플레이하는 게임 중 하나이며 2016년 기준 월 플레이어 수 1억 명 이상을 달성했고, 2019년 8월 기준 하루 전 세계 서버의 피크 시간 동시 접속자 수를 합치면 800만 명 이상이다. 대한민국에서는 정식 출시된 2011년 이후부터 PC방 점유율 1위를 석권한 최고의 인기 PC 게임으로, 주로 청소년층과 젊은 성인층을 중심으로 꾸준히 사랑받고 있다. <br />\
+    또한 전 세계 e스포츠 대회 중 가장 많은 시청자 수 기록을 보유 중인 리그 오브 레전드 월드 챔피언십과 각 지역 리그 등 수많은 e스포츠 대회가 많이 개최되는 중이다. 그리고 2018 자카르타·팔렘방 아시안 게임에서 공식 시범 종목으로 채택되었다.\
+    <hr />\
+    <div class='head'>관련 영상</div>\
+    <hr />\
+    <ul class='cover'>\
+        <li>\
+            <iframe width='450' height='300' src='https://www.youtube.com/embed/IldpxvOzoOE?si=r2O85h5dPqsqTy7N' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share' referrerpolicy='strict-origin-when-cross-origin' allowfullscreen></iframe>\
+            <iframe width='450' height='300' src='https://www.youtube.com/embed/T6cMmQoJ_4A?si=wIfZcDEaaj7C7r-i' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share' referrerpolicy='strict-origin-when-cross-origin' allowfullscreen></iframe>\
+            <iframe width='450' height='300' src='https://www.youtube.com/embed/F5aP6eSZ6gc?si=bxdYSvXLoyiMRuo5' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share' referrerpolicy='strict-origin-when-cross-origin' allowfullscreen></iframe>\
+        </li>\
+    </ul>"
+});
+
+document.getElementById("image2").addEventListener("click", () => {
+    string = "";
+    string = document.getElementById("extra").innerHTML = "<div class='head'>개임 소개</div><hr />\
+    워크래프트 3의 유즈맵 디펜스 오브 디 에인션트의 파생작인 도타 올스타즈 기반으로 밸브 코퍼레이션이 독립된 게임으로 제작한 AOS 게임.<br />\
+    <hr />\
+    <div class='head'>관련 영상</div>\
+    <hr />\
+    <ul class='cover'>\
+        <li>\
+            <iframe width='450' height='300' src='https://www.youtube.com/embed/GQL8YDs6uNA?si=KlFg85dyWQ-yylOH' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share' referrerpolicy='strict-origin-when-cross-origin' allowfullscreen></iframe>\
+            <iframe width='450' height='300' src='https://www.youtube.com/embed/_fdkxrq1Xyc?si=SLmCfPI7TfhoSTD3' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share' referrerpolicy='strict-origin-when-cross-origin' allowfullscreen></iframe>\
+            <iframe width='450' height='300' src='https://www.youtube.com/embed/ColfvV3PGvc?si=uYgUKSJUfHjl4t2o' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share' referrerpolicy='strict-origin-when-cross-origin' allowfullscreen></iframe>\
+        </li>\
+    </ul>"
+});
+
+document.getElementById("image3").addEventListener("click", () => {
+    string = "";
+    string = document.getElementById("extra").innerHTML = "<div class='head'>개임 소개</div><hr />\
+    블리자드 엔터테인먼트에서 제작한 MOBA 게임.<br />\
+    히오스에는 전장 내 아이템(상점) 개념이 없다. 히오스는 성장에 따라 기존 스킬을 강화하거나 새로운 보조 스킬을 배우는, 선택에 따라 계속해서 변화를 줄 수 있는 특성 시스템을 따른다. 특성은 타 게임의 아이템과 달리 한번 선택하면 반환이 불가능하며, 일반적으로는 팀 레벨 1, 4, 7, 10, 13, 16, 20에서 하나를 선택할 수 있다. 특성은 다양하며, 각 영웅만의 독점 특성들이나 동일한 효과를 갖는 공용 특성이 있다. 일반적으로 10레벨에는 궁극기(R)를 선택하며, 또, 20레벨에는 '폭풍 특성'이라고 불리는 궁극기 강화 혹은 그에 준하는 매우 강력한 특성을 선택할 수 있다. <br />\
+    히오스의 의도는 특성 선택지의 다양화로 개성있는 플레이타입과 전략을 구사하도록 하려는 것이었다. 그러나 실질적으로 아이템이 없는 히오스에서의 영웅은 특성의 영향에 크게 좌우되는 경우가 많아서, 일관되지 않은 특성 개발은 오히려 이도저도 아닌 캐릭터가 되어버린다. 사실상, 특성은 보기에만 다채로울 뿐 함정과 정석이 정해져 있다. 따라서 각 특성의 효과와 정석 특성트리를 사전에 암기하여야 하며, 오히려 아이템이 있는 타 MOBA보다 자유도가 낮다는 평가를 받는다.\
+    <hr />\
+    <div class='head'>관련 영상</div>\
+    <hr />\
+    <ul class='cover'>\
+        <li>\
+            <iframe width='450' height='300' src='https://www.youtube.com/embed/YE_Us0u02pA?si=cOBnz8xRBey4rbdh' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share' referrerpolicy='strict-origin-when-cross-origin' allowfullscreen></iframe>\
+            <iframe width='450' height='300' src='https://www.youtube.com/embed/m0CwhGlc90g?si=pe7QVBLZjQjjhda5' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share' referrerpolicy='strict-origin-when-cross-origin' allowfullscreen></iframe>\
+            <iframe width='450' height='300' src='https://www.youtube.com/embed/o9GQVUbQDkY?si=5HrSjv7fgZjYvIm9' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share' referrerpolicy='strict-origin-when-cross-origin' allowfullscreen></iframe>\
+        </li>\
+    </ul>"
+});
+
+document.getElementById("image4").addEventListener("click", () => {
+    string = "";
+    string = document.getElementById("extra").innerHTML = "<div class='head'>개임 소개</div><hr />\
+    넵튠의 자회사 님블뉴런에서 만든 쿼터뷰 배틀로얄 게임이다.<br />\
+    약칭은 이터널 리턴을 줄인 이리인데 초창기에는 '블랙서바이벌: 영원회귀'로 개발되다 나중에 '영원회귀: 블랙서바이벌'으로 바뀌는 등 이름이 몇 번 바뀐 적이 있어 그 당시의 약칭이었던 영회나 블서도 드물게 사용된다. 최종적으로는 영원회귀를 따서 각각 영어로 이터널(영원히) 리턴(회귀)으로 하였다. 리턴이 돈다는 뜻도 있어서 공식 영상에서는 이런식으로 장난을 치기도 하였다. # 2025년 6월 27일 텐센트의 퍼블리싱으로 중국 서비스가 시작되는데 게임 명칭이 永恒轮回(영원회귀)로 확정되어 개발초기 프로젝트명으로 쓰이던 이름도 결국 사용하게 된 셈이 되었다.<br />\
+    여담으로 블서라고 하면 원작인 블랙서바이벌의 약칭과 완전히 겹치지만 원작이 나온지 오래된 게임이고 현재는 서비스도 종료되어 이제 블서라고 하면 이터널 리턴을 가리키는 경우가 많다.<br />\
+    게임당 최대 24명의 플레이어(3 x 8 = 24)가 참여하여 파밍한 물품들을 이용해 장비와 음식을 만들고 활용하는 배틀로얄 게임으로, 하이퍼루프를 이용한 순간이동과 트랩과 오브젝트 등을 통한 다양한 전략으로 최후의 1팀을 가려내는 게임이다. 한 팀에 세 명이 있으므로, 각 팀은 3인조, 트리오, 3인의 법칙 등으로 표현된다.<br />\
+    전작 블랙서바이벌처럼 배틀로얄 게임인 것은 마찬가지지만, 이 파밍 방식을 그대로 따오면서도 전투는 탑뷰와 쿼터뷰 사이의 시점에서 키보드, 마우스로 조작하는 방식으로 바뀌었다. 쉽게 말해서 롤 같은 쿼터뷰 액션 게임의 조작방식이다. 결과적으로 앞서 존재한 배틀라이트 로얄과 흡사한 쿼터뷰+ 배틀로얄 방식의 생존 게임이지만, 파밍 루트를 짜거나 탐색이나 제작 등으로 레벨업을 해야 하는 등 세세한 부분에선 느낌이 많이 다르다.<br />\
+    인벤 기사에서 밝히길, 본작의 개발은 과거 C9, 검은 사막 등 온라인 PC게임과 모바일 게임을 만든 경험이 있는 전 오올블루 개발팀이 독립적으로 해오고 있었으며, 전작인 블랙서바이벌과는 기본 설정을 공유하는 일종의 평행세계라고 한다.<br />\
+    2020년 7월 29일 OBT가 시작됐으며 10월 14일에 얼리 액세스가 시작됐다. 2021년 3월 기준으로 스팀 피크타임 동시접속자 2만명 근처를 기록하며 얼리 액세스 상태임에도 나쁘지 않은 성적을 보이고 있다. 2021년 2월 0.26.0 패치를 기점으로 웰비아닷컴의 Uncheater가 안티 치트 솔루션으로 적용되었다. 2021년 3월, 님블뉴런이 카카오게임즈와 퍼블리싱 계약을 체결하면서 한국 퍼블리싱을 카카오게임즈가 담당하게 되었다. 2021년 5월 27일, 게임명을 '영원회귀: 블랙서바이벌'(Eternal Return: Black Survival)에서 부제인 블랙서바이벌을 뺀 '이터널 리턴'으로 변경했다.[8] 그 동안 이 게임은 영원회귀, 블랙서바이벌, 영블, 블서, ER 등 다양한 이름으로 불려왔다. 이에, 카카오게임즈는 정식 출시 이전에 한국 및 해외 표기법을 통일하고자 게임명을 이와 같이 부르기로 했다. 2021년 7월 22일, 카카오 서비스가 오픈하였다. 신인 걸그룹 aespa를 광고 모델로 채택하였다. 2021년 11월, 26회 대한민국 게임대상에서 우수상과 인기게임상을 수상했다. 2023년 7월 20일, 3년간의 얼리 액세스 버전 운영을 종료하고 이터널 리턴 1.0으로 정식 버전을 출시하게 되었다.\
+    <hr />\
+    <div class='head'>관련 영상</div>\
+    <hr />\
+    <ul class='cover'>\
+        <li>\
+            <iframe width='450' height='300' src='https://www.youtube.com/embed/rgbA30jIVTU?si=7HpWUSMmLa1yfd2L' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share' referrerpolicy='strict-origin-when-cross-origin' allowfullscreen></iframe>\
+            <iframe width='450' height='300' src='https://www.youtube.com/embed/7jPWFHxYRa8?si=GefF7Mq2dzK31VpO' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share' referrerpolicy='strict-origin-when-cross-origin' allowfullscreen></iframe>\
+            <iframe width='450' height='300' src='https://www.youtube.com/embed/xQPXisFZNJw?si=FMY1l8UkRLJN6Mgi' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share' referrerpolicy='strict-origin-when-cross-origin' allowfullscreen></iframe>\
+        </li>\
+    </ul>"
+});
---
+++ rhythm.js
@@ -0,0 +1,75 @@
+let string;
+
+document.getElementById("image1").addEventListener("click", () => {
+    string = "";
+    string = document.getElementById("extra").innerHTML = "<div class='head'>개임 소개</div><hr />\
+    <div>DJMAX RESPECT V는 한국의 로키 스튜디오에서 개발하고 네오위즈에서 퍼블리싱하는 DJMAX 시리즈의 PC 리듬게임으로, 2017년 PS4 플랫폼으로 출시된 DJMAX RESPECT에 추가 콘텐츠를 포함시킨 이식작이다. 공식 약칭은 DMRV이다.<br />\
+    2019년 11월 18일 최초 공개되어 2019년 12월 19일 얼리 액세스를 시작했으며, 2020년 3월 12일 정식 서비스를 시작했다. 2022년 7월 7일 Xbox Game Pass 플랫폼에도 정식 출시되었다.</div>\
+    <hr />\
+    <div class='head'>관련 영상</div>\
+    <hr />\
+    <ul class='cover'>\
+        <li>\
+            <iframe width='450' height='300' src='https://www.youtube.com/embed/IkygdWqvOV8?si=Tr7GZ_gbTyDPywD0' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share' referrerpolicy='strict-origin-when-cross-origin' allowfullscreen></iframe>\
+            <iframe width='450' height='300' src='https://www.youtube.com/embed/azWYOG8yXQw?si=nTGp6d7ER19Yijh1' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share' referrerpolicy='strict-origin-when-cross-origin' allowfullscreen></iframe>\
+            <iframe width='450' height='300' src='https://www.youtube.com/embed/FISrD1fEEo8?si=Fe-5NwpHUYQX5SIj' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share' referrerpolicy='strict-origin-when-cross-origin' allowfullscreen></iframe>\
+        </li>\
+    </ul>"
+});
+
+document.getElementById("image2").addEventListener("click", () => {
+    string = "";
+    string = document.getElementById("extra").innerHTML = "<div class='head'>개임 소개</div><hr />\
+    말레이시아에 소재한 게임 회사인 7th Beat Games에서 개발한 원터치 리듬 게임.<br />\
+    한국 유저들 사이에서는 제목이 너무 길어서 주로 '얼불춤'으로 불린다. 순서가 불과 얼음임에도 얼불춤인 이유는 얼불노 때문도 있지만 출시 초기에는 많은 유저들이 풀네임 순서 그대로 '불얼춤'이라고 줄여 불렀었으나 어감이 좀 그렇다는 문제로(...) 얼불춤으로 불리게 되었다. 해외에서는 약칭인 ADOFAI로 부른다.<br />\
+    48시간 내로 게임을 만드는 대회인 Ludum dare 30의 일환으로 만들어진 게임으로, 개발사의 또 다른 게임인 Rhythm Doctor처럼 사이트에서 데모 버전을 공개한 후 4년 만에 스팀에서 정식버전으로 출시되었다. 가격은 스팀버전 6,500원, itch.io버전 3.99달러(약 4600원), 모바일은 구글 플레이 스토어 기준으로 2,200원, 애플 앱스토어 기준으로 3,300원 이다. <br />\
+    게임명은 유명한 소설 시리즈인 얼음과 불의 노래를 모티브로 한 듯하다. <br />\
+    이후 Nintendo Switch 이식이 확정되었다. 스팀 버전의 Neo Cosmos DLC의 레벨과 추천 레벨을 포함한 모든 레벨이 포함 될 예정. 출시는 원래 2024년 가을에 예정되어 있었으나, 2025년으로 연기되었다. <br />\
+    <hr />\
+    <div class='head'>관련 영상</div>\
+    <hr />\
+    <ul class='cover'>\
+        <li>\
+            <iframe width='450' height='300' src='https://www.youtube.com/embed/p9NuERYrzIo?si=SIaC3xBMAI-8cLi_' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share' referrerpolicy='strict-origin-when-cross-origin' allowfullscreen></iframe>\
+            <iframe width='450' height='300' src='https://www.youtube.com/embed/FDHxZnTpmpk?si=9Xi4CSE_986pPx_w' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share' referrerpolicy='strict-origin-when-cross-origin' allowfullscreen></iframe>\
+            <iframe width='450' height='300' src='https://www.youtube.com/embed/jxYV-kdt3H0?si=Gmk4B-qdlIpyuNJ0' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share' referrerpolicy='strict-origin-when-cross-origin' allowfullscreen></iframe>\
+        </li>\
+    </ul>"
+})
+
+document.getElementById("image3").addEventListener("click", () => {
+    string = "";
+    string = document.getElementById("extra").innerHTML = "<div class='head'>개임 소개</div><hr />\
+    호주의 peppy라는 개발자가 응원단 시리즈의 게임 방식을 따와서 만든 오프라인&온라인 리듬 게임. osu!라는 제목은 응원단 시리즈의 응원구호인 '옷스!(押忍!)' 에서 따온 것이다.<br />\
+    현재는 주객이 완전히 전도되어서 응원단 시리즈는 몰라도 이 게임은 아는 사람들이 많아졌으며 osu!가 응원단 시리즈의 정식 후속작이라 생각하는 사람은 물론 응원단 시리즈가 osu!의 짝퉁이라고 생각하는 사람도 늘어났다. 이러한 점은 아브의 응원단 1편 실황 플레이 영상 코멘트에서 잘 드러났다. 이는 원작인 응원단 시리즈가 2007년 응원단2를 끝으로 더 안나오기에 게임 인지도가 갈수록 잊혀져가는 것도 한몫했다.<br />\
+    <hr />\
+    <div class='head'>관련 영상</div>\
+    <hr />\
+    <ul class='cover'>\
+        <li>\
+            <iframe width='450' height='300' src='https://www.youtube.com/embed/336BlGiQar4?si=SCHLSHTRHAYe0f3o' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share' referrerpolicy='strict-origin-when-cross-origin' allowfullscreen></iframe>\
+            <iframe width='450' height='300' src='https://www.youtube.com/embed/OvCbKP_ox7o?si=WSGiyt2jarPNcE3o' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share' referrerpolicy='strict-origin-when-cross-origin' allowfullscreen></iframe>\
+            <iframe width='450' height='300' src='https://www.youtube.com/embed/l-VPCGl0qy4?si=sDp82grjG4sdioip' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share' referrerpolicy='strict-origin-when-cross-origin' allowfullscreen></iframe>\
+        </li>\
+    </ul>"
+})
+
+document.getElementById("image4").addEventListener("click", () => {
+    string = "";
+    string = document.getElementById("extra").innerHTML = "<div class='head'>개임 소개</div><hr />\
+    A Dance of Fire and Ice를 제작한 7th Beat Games에서 제작한 리듬 게임. 2021년 2월 26일에 스팀에서 앞서 해보기로 출시되었다. BIC나 G-STAR에서도 출품되었으며 공식적으로 한국어를 지원한다.<br />\
+    <hr />\
+    <div class='head'>관련 영상</div>\
+    <hr />\
+    <ul class='cover'>\
+        <li>\
+            <iframe width='450' height='300' src='https://www.youtube.com/embed/mV7p1NXEmB0?si=fEOCHg8icAbz2M84' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share' referrerpolicy='strict-origin-when-cross-origin' allowfullscreen></iframe>\
+            <iframe width='450' height='300' src='https://www.youtube.com/embed/xMz2dHgKZdw?si=wSGnbUsib-OzMXy7' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share' referrerpolicy='strict-origin-when-cross-origin' allowfullscreen></iframe>\
+            <iframe width='450' height='300' src='https://www.youtube.com/embed/gBcvKobh9N0?si=Qcg45ORQoeiuucAK' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share' referrerpolicy='strict-origin-when-cross-origin' allowfullscreen></iframe>\
+        </li>\
+    </ul>"
+})
+
+document.getElementsByName("rhythm").addEventListener("click", () => {
+    href = "rhythm.html";
+})
